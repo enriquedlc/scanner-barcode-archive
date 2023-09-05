42ba816dc3fa3576de8bc6534fe194c812b92dcc
Enrique De La Concepción <101868281+enriquedlc@users.noreply.github.com>
2023-09-06 00:58:32 +0100

    :sparkles: adding article deletion

--- src/components/article-item/article-item-styles.ts
+++ src/components/article-item/article-item-styles.ts
@@ -1,19 +1,29 @@
 import { StyleSheet } from 'react-native'
 
 export const articleItemStyles = StyleSheet.create({
+    articleItemContainer: {
+        width: '100%',
+        alignItems: 'center',
+        marginVertical: 10,
+    },
     articleItem: {
         height: 100,
         minWidth: '90%',
-        backgroundColor: 'lightblue',
-        marginTop: 15,
+        backgroundColor: 'white',
         borderRadius: 10,
         alignSelf: 'center',
         shadowColor: 'black',
-        shadowOffset: { width: 0, height: 5 },
-        shadowOpacity: 0.5,
+        shadowOffset: { width: 0, height: 7 },
+        shadowOpacity: 0.3,
         shadowRadius: 10,
         elevation: 50,
-        marginBottom: 10,
-
+    },
+    iconContainer: {
+        height: 100,
+        minWidth: 100,
+        position: 'absolute',
+        right: "10%",
+        justifyContent: 'center',
+        alignItems: 'center',
     }
 });
--- src/components/article-item/article-item.tsx
+++ src/components/article-item/article-item.tsx
@@ -1,10 +1,15 @@
 import React from "react";
-import { Text, ViewToken } from "react-native";
+import { Text, ViewToken, View } from "react-native";
 import Animated, {
   SharedValue,
+  useAnimatedGestureHandler,
   useAnimatedStyle,
+  useSharedValue,
   withTiming,
 } from "react-native-reanimated";
+import { PanGestureHandler } from "react-native-gesture-handler";
+import { PanGestureHandlerGestureEvent } from "react-native-gesture-handler";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 import { Article } from "../../types/article";
 
@@ -14,6 +19,7 @@ interface ArticleItemProps {
   item: Article;
   viewableArticles: SharedValue<ViewToken[]>;
 }
+// TODO: refactor -> use memo?
 
 export const ArticleItem: React.FC<ArticleItemProps> = React.memo(
   ({ item, viewableArticles }) => {
@@ -34,12 +40,46 @@ export const ArticleItem: React.FC<ArticleItemProps> = React.memo(
       };
     }, [viewableArticles]);
 
+    const translateX = useSharedValue(0);
+
+    const panGesture = useAnimatedGestureHandler<PanGestureHandlerGestureEvent>(
+      {
+        onActive: (event) => {
+          translateX.value = event.translationX;
+        },
+        onEnd: () => {
+          translateX.value = withTiming(0);
+        },
+      }
+    );
+
+    const reanimatedGestureStyle = useAnimatedStyle(() => ({
+      transform: [
+        {
+          translateX: translateX.value,
+        },
+      ],
+    }));
+
     return (
-      <Animated.View style={[articleItemStyles.articleItem, reanimatedStyle]}>
-        <Text>{item.name}</Text>
-        <Text>{item.barcode}</Text>
-        <Text>{item.updatedAt}</Text>
-      </Animated.View>
+      <View style={articleItemStyles.articleItemContainer}>
+        <View style={articleItemStyles.iconContainer}>
+          <FontAwesome5 name="trash-alt" size={24} color="red" />
+        </View>
+        <PanGestureHandler onGestureEvent={panGesture}>
+          <Animated.View
+            style={[
+              articleItemStyles.articleItem,
+              reanimatedStyle,
+              reanimatedGestureStyle,
+            ]}
+          >
+            <Text>{item.name}</Text>
+            <Text>{item.barcode}</Text>
+            <Text>{item.updatedAt}</Text>
+          </Animated.View>
+        </PanGestureHandler>
+      </View>
     );
   }
 );
