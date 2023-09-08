8397370230856ed3512b1354919aed185acc46b0
Enrique De La Concepción <101868281+enriquedlc@users.noreply.github.com>
2023-09-08 01:56:00 +0100

    refactor:hammer: extracted footer buttons to a component

--- src/components/profile/profile-footer/profile-footer.tsx
+++ src/components/profile/profile-footer/profile-footer.tsx
@@ -1,43 +1,23 @@
-import { Image, Text, TouchableOpacity, View, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 
 import { INFO_CIRCLE, LOGOUT } from "../../../../assets/profile-images";
 import { BLUE_PALLETE } from "../../../constants/colors/colors";
 
-import { profileStyles } from "../../../screens/profile-screen/profile-styles";
+import { ProfileFooterButton } from "./profile-footer-button";
 
 export function ProfileFooter() {
   return (
     <View style={profileFooterStyles.profileFooterContainer}>
-      <TouchableOpacity
-        style={{
-          flexDirection: "row",
-          alignSelf: "center",
-          borderWidth: 2,
-          borderColor: BLUE_PALLETE.BLUE,
-          paddingHorizontal: 10,
-          paddingTop: 10,
-          paddingBottom: 2,
-          borderRadius: 10,
-        }}
-      >
-        <Text style={profileStyles.profileDefaultLabel}>Atribuciones</Text>
-        <Image source={INFO_CIRCLE} style={profileStyles.profileIcon} />
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={{
-          flexDirection: "row",
-          alignSelf: "center",
-          borderWidth: 2,
-          borderColor: "red",
-          paddingHorizontal: 10,
-          paddingTop: 10,
-          paddingBottom: 2,
-          borderRadius: 10,
-        }}
-      >
-        <Text style={profileStyles.profileDefaultLabel}>Cerrar Sesión</Text>
-        <Image source={LOGOUT} style={profileStyles.profileIcon} />
-      </TouchableOpacity>
+      <ProfileFooterButton
+        icon={INFO_CIRCLE}
+        label="Atribuciones"
+        outlineColor={BLUE_PALLETE.BLUE}
+      />
+      <ProfileFooterButton
+        icon={LOGOUT}
+        label="Cerrar Sesión"
+        outlineColor="red"
+      />
     </View>
   );
 }
