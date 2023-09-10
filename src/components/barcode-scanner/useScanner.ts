9cb3411bff68c96a1906ff8decde5dc01569520e
Enrique De La Concepción <101868281+enriquedlc@users.noreply.github.com>
2023-09-11 00:20:37 +0100

    :sparkles: toast indicanting the scanning process

--- src/components/barcode-scanner/useScanner.ts
+++ src/components/barcode-scanner/useScanner.ts
@@ -1,21 +1,32 @@
 import { useState, useEffect } from 'react'
 import { BarCodeScanner } from 'expo-barcode-scanner';
 
+import { useShowToast } from '../../hooks/useShowToast';
+
 import { ScannedData } from '../../types/types';
 
 export const useScanner = () => {
+
+    const { showToast } = useShowToast();
+
     const [hasPermission, setHasPermission] = useState(false);
     const [scannedData, setScannedData] = useState("");
 
+    console.log('has persmission', hasPermission)
+
     useEffect(() => {
+        if (hasPermission) return;
         (async () => {
             const { status } = await BarCodeScanner.requestPermissionsAsync();
             setHasPermission(status === "granted");
         })();
-    }, []);
+    }, [hasPermission]);
 
     const handleBarCodeScanned = (scannedData: ScannedData) => {
         setScannedData(scannedData.data);
+
+        if (scannedData) showToast("info", "Artículo escaneado", "artículo escaneado correctamente", 5000);
+
         console.log(`Type: ${scannedData.type}`);
         console.log(`Data: ${scannedData.data}`);
     };
--- src/hooks/useShowToast.ts
+++ src/hooks/useShowToast.ts
@@ -4,13 +4,13 @@ type ToastType = "success" | "error" | "info";
 
 export const useShowToast = () => {
 
-    const showToast = (type: ToastType, title: string, message: string) => {
+    const showToast = (type: ToastType, title: string, message: string, visibilityTime?: number) => {
         Toast.show({
             type,
             position: "top",
             text1: title,
             text2: message,
-            visibilityTime: 4000,
+            visibilityTime: 4000 ?? visibilityTime,
             autoHide: true,
             topOffset: 60,
         });
