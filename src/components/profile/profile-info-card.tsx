f1a7efbc05ad494c7772cbf5c8065befb707a35d
Enrique De La Concepción <101868281+enriquedlc@users.noreply.github.com>
2023-10-05 22:59:14 +0100

    :package: removed unused libraries

--- src/components/profile/profile-info-card.tsx
+++ src/components/profile/profile-info-card.tsx
@@ -8,32 +8,32 @@ import { USER_PROFILE_IMAGES } from "../../../assets";
 import { profileStyles } from "../../screens/profile-screen/profile-styles";
 
 export function ProfileInfoCard() {
-  const user = useUserAuthStore((state) => state.user);
+	const user = useUserAuthStore((state) => state.user);
 
-  return (
-    <View style={profileStyles.profileCard}>
-      <View style={profileStyles.photo}>
-        <Image
-          source={USER_PROFILE_IMAGES.BLUE_GRADIENT_USER}
-          alt="user photo"
-          style={{
-            width: 50,
-            height: 50,
-            objectFit: "contain",
-            tintColor: "black",
-          }}
-        />
-        <View style={profileStyles.editPhotoButton}>
-          {/* TODO: implement changing profile photo */}
-          <TouchableOpacity onPress={() => console.log("change photo")}>
-            <FontAwesome5 name="camera" size={16} color="white" />
-          </TouchableOpacity>
-        </View>
-      </View>
-      <View style={profileStyles.infoLabelContainer}>
-        <Text style={profileStyles.name}>{user?.username}</Text>
-        <Text style={profileStyles.email}>{user?.email} </Text>
-      </View>
-    </View>
-  );
+	return (
+		<View style={profileStyles.profileCard}>
+			<View style={profileStyles.photo}>
+				<Image
+					source={USER_PROFILE_IMAGES.BLUE_GRADIENT_USER}
+					alt="user photo"
+					style={{
+						width: 50,
+						height: 50,
+						objectFit: "contain",
+						tintColor: "black",
+					}}
+				/>
+				<View style={profileStyles.editPhotoButton}>
+					{/* TODO: implement changing profile photo */}
+					<TouchableOpacity onPress={() => console.log("change photo")}>
+						<FontAwesome5 name="camera" size={16} color="white" />
+					</TouchableOpacity>
+				</View>
+			</View>
+			<View style={profileStyles.infoLabelContainer}>
+				<Text style={profileStyles.name}>{user?.username}</Text>
+				<Text style={profileStyles.email}>{user?.email} </Text>
+			</View>
+		</View>
+	);
 }
