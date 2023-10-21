bf810dd2d7ef2fff42bbe3733723d0ca921191ff
Enrique De La Concepción <101868281+enriquedlc@users.noreply.github.com>
2023-10-21 16:50:46 +0100

    :sparkles: changed article item icon

--- src/components/article-item/components/article-item-header.tsx
+++ src/components/article-item/components/article-item-header.tsx
@@ -1,15 +1,21 @@
-import { Image, Text, View } from "react-native";
+import { Image, ImageProps, Text, View } from "react-native";
 
 import { Article } from "../../../types/article";
 
-import { TOOLS } from "../../../../assets";
-
+import { CATEGORY_ITEM_LIST_ICONS } from "../../../constants/category-item-list-icons/category-item-list-icons";
 import { BarcodeCategoryTags } from "../../barcode-category-tags/barcode-category-tags";
 import { articleItemStyles } from "../article-item-styles";
 
 export const ArticleItemHeader = ({ articleName, barcode, categoryName }: Partial<Article>) => (
 	<View style={articleItemStyles.articleItemHeader}>
-		<Image style={articleItemStyles.articleHeaderIcon} source={TOOLS.toolBox} alt="tool-box" />
+		<Image
+			style={articleItemStyles.articleHeaderIcon}
+			source={
+				CATEGORY_ITEM_LIST_ICONS.find((icon) => icon.categoryName === categoryName)
+					?.icon as ImageProps
+			}
+			alt="tool-box"
+		/>
 		<View style={articleItemStyles.articleItemHeaderDescription}>
 			<Text style={articleItemStyles.articleItemTitle}>{articleName}</Text>
 			<BarcodeCategoryTags
