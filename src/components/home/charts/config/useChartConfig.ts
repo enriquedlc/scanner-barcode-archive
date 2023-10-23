972165d293de59abbd7f2f90215f81680cdf59eb
Enrique De La Concepción <101868281+enriquedlc@users.noreply.github.com>
2023-10-23 18:24:17 +0100

    :sparkles: added server data to the charts

--- src/components/home/charts/config/useChartConfig.ts
+++ src/components/home/charts/config/useChartConfig.ts
@@ -1,13 +1,20 @@
 import { Dimensions } from "react-native";
+import { AbstractChartConfig } from "react-native-chart-kit/dist/AbstractChart";
 
 export const useChartConfig = () => {
 	// here we can instantiate the preffered user theme
 
 	const width = Dimensions.get("screen").width - 16;
-	const height = 230;
+	const height = 220;
 
-	const chartConfigs = [
+	const chartConfigs: AbstractChartConfig[] = [
 		{
+			propsForLabels: {
+				dy: 4,
+				skewX: -5,
+				fontWeight: "bold",
+			},
+
 			backgroundColor: "#022173",
 			backgroundGradientFrom: "#022173",
 			backgroundGradientTo: "#1b3fa0",
@@ -23,5 +30,12 @@ export const useChartConfig = () => {
 		marginHorizontal: 10,
 	};
 
-	return { chartConfigs, graphStyle, width, height };
+	const monthsToDisplay = 6;
+	const monthLabels = Array.from({ length: monthsToDisplay }, (_, i) => {
+		const date = new Date();
+		date.setMonth(date.getMonth() - i);
+		return date.toLocaleString("default", { month: "short" });
+	}).reverse();
+
+	return { chartConfigs, graphStyle, width, height, monthLabels };
 };
--- src/components/home/charts/scanned-articles-history-line-chart.tsx
+++ src/components/home/charts/scanned-articles-history-line-chart.tsx
@@ -1,27 +1,76 @@
+import { useEffect } from "react";
+import { ActivityIndicator } from "react-native";
 import { LineChart } from "react-native-chart-kit";
+import { ChartData } from "react-native-chart-kit/dist/HelperTypes";
 
+import { useArticlesStore } from "../../../store/articles";
+import { ChartTitle } from "./chart-title";
 import { useChartConfig } from "./config/useChartConfig";
 
 import { CHART_ICONS } from "../../../../assets";
 
-import { ChartTitle } from "./chart-title";
-import { MOCK_CHART_DATA } from "./data/mock-chart-data";
-
 export function ScannedArticlesHistoryLineChart() {
 	const { chartConfigs, graphStyle, height, width } = useChartConfig();
+
+	const { articles } = useArticlesStore((state) => state);
+
+	const findArticlesCreatedPerDay = (): { labels: string[]; values: number[] } => {
+		const articlesCreatedPerDay: { [key: string]: number } = {};
+		// biome-ignore lint/complexity/noForEach: <explanation>
+		articles?.forEach((article) => {
+			if (article.createdAt) {
+				const date = new Date(article.createdAt);
+				const month = date.getMonth();
+				const day = date.getDate();
+				const dateKey = `${day}/${month}`;
+				if (articlesCreatedPerDay[dateKey]) {
+					articlesCreatedPerDay[dateKey] += 1;
+				} else {
+					articlesCreatedPerDay[dateKey] = 1;
+				}
+			}
+		});
+		const sortedArticlesCreatedPerDay = Object.entries(articlesCreatedPerDay).sort(
+			(a, b) => new Date(a[0]).getTime() - new Date(b[0]).getTime(),
+		);
+		const labels = sortedArticlesCreatedPerDay.map((article) => article[0]).reverse();
+		const values = sortedArticlesCreatedPerDay.map((article) => article[1]).reverse();
+		return { labels, values };
+	};
+
+	// biome-ignore lint/nursery/useExhaustiveDependencies: <explanation>
+	useEffect(() => {
+		findArticlesCreatedPerDay();
+	}, [articles]);
+
+	const data: ChartData = {
+		labels: findArticlesCreatedPerDay()?.labels,
+		datasets: [
+			{
+				data: findArticlesCreatedPerDay()?.values,
+				color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
+				strokeWidth: 2, // optional
+			},
+		],
+	};
 
 	return (
 		<>
 			<ChartTitle icon={CHART_ICONS.CHART_HISTOGRAM} title="Histórico de artículos" />
-			<LineChart
-				data={MOCK_CHART_DATA}
-				width={width}
-				height={height}
-				chartConfig={chartConfigs[0]}
-				bezier={true}
-				style={graphStyle}
-				key="1"
-			/>
+			{/* biome-ignore lint/nursery/noGlobalIsNan: <explanation> */}
+			{isNaN(findArticlesCreatedPerDay().values[0]) ? (
+				<ActivityIndicator size={35} style={{ paddingTop: "10%", alignSelf: "center" }} />
+			) : (
+				<LineChart
+					data={data}
+					width={width}
+					height={height}
+					chartConfig={chartConfigs[0]}
+					bezier={true}
+					style={graphStyle}
+					key="1"
+				/>
+			)}
 		</>
 	);
 }
